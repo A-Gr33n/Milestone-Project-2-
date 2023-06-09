417c848e24c902955b5f51ef752fd0eb624dd08b
A-Gr33n <aarongreen193@hotmail.com>
2023-06-09 13:59:46 +0000

    added reset function and made page responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,6 +45,7 @@ let firstCard = null ;
 let secondCard = null;
 let moves = 0 ;
 let score = 0 ;
+let allowClick = true;
 // let noOfCardOpen ;
 
 //Start Game function
@@ -52,11 +53,14 @@ function startGame() {
   displayAnimals();
   addEventListenersToCard();
   }
+
 function addEventListenersToCard() {
   document.querySelectorAll(".card").forEach(function(card){
     card.addEventListener("click", flipCard);
   });
-};//Animal display 
+};
+
+//Animal display 
 function displayAnimals() {
   let animalsList = [...ANIMALS_CONFIG, ...ANIMALS_CONFIG];
   animalsList = shuffleArray(animalsList);
@@ -69,63 +73,95 @@ function displayAnimals() {
               </div>`;
   });
   document.getElementById("game-container").innerHTML = animalsHTML;
-};//Shuffle cards everytime new game starts 
+};
+
+//Shuffle cards everytime new game starts 
 function shuffleArray(array){
   return array.sort( () => .5 - Math.random() );
-};// Card flips when user clicks a card 
+};
+
+// Card flips when user clicks a card 
 function flipCard(e) {
-    e.target.classList.toggle("flipped");
-    if(!firstCard) {
-        //First card selected
-        firstCard = e.target;
-    }else{
-         //Second card selected
-        secondCard = e.target;
+  if(!allowClick) {
+    return;
+  }
+
+  e.target.classList.toggle("flipped");
+  if(!firstCard) {
+      //First card selected
+      firstCard = e.target;
+  }else{
+        //Second card selected
+      secondCard = e.target;
+  }
+  if(firstCard && secondCard) {
+      allowClick = false;
+      checkForMatch();
+      incrementMoves();
     }
-    if(firstCard && secondCard) {
-        checkForMatch();
-        incrementMoves();
-     }
-};//On 2nd click check if both card animal matches
+};
+
+
+function setMsg(msg) {
+  document.getElementById('msg').innerText = msg;
+}
+
+//On 2nd click check if both card animal matches
 function checkForMatch(){ 
     const firstCardId = firstCard.dataset.id;
     const secondCardId = secondCard.dataset.id;
     if(firstCardId === secondCardId) {
-        incrementScore()
-        firstCard = null;
-        secondCard = null;
-       setTimeout(()=>{
-        alert("Good job, it's a match!");
-       },500)
-     }else{
+      incrementScore()
+      firstCard = null;
+      secondCard = null;
+      setMsg("Good job, it's a match!");
+      setTimeout(()=>{
+        allowClick= true;
+        setMsg("");
+      },500)
+    } else{
         //  if two cards don't match
         // Else, flip back both cards
+        setMsg("ah sorry no match, try again");
         const timeoutRef = setTimeout(()=> {
             firstCard.classList.toggle("flipped");
             secondCard.classList.toggle("flipped");
             firstCard = null;
             secondCard = null;
+            allowClick= true;
+            setMsg("");
             clearTimeout(timeoutRef);
-            alert("ah sorry no match, try again");
         }, 1000)
     };
 };
- function incrementScore() {
-     const scoreNode = document.getElementById("score");
-    scoreNode.innerText = parseInt(scoreNode.innerText) + 1;
-    // increment matched value by one
-    // eachScore = 1
-};
-function incrementMoves() {
-     const moves = document.getElementById("moves-count");
-     moves.innerText = parseInt(moves.innerText) +1;
-     // increment move when both cards are flipped
-     // each move = 1
-   };
 
+
+function incrementScore() {
+  const scoreNode = document.getElementById("score");
+  scoreNode.innerText = parseInt(scoreNode.innerText) + 1;
+  // increment matched value by one
+  // eachScore = 1
+};
+
+
+function incrementMoves() {
+  const moves = document.getElementById("moves-count");
+  moves.innerText = parseInt(moves.innerText) +1;
+  // increment move when both cards are flipped
+  // each move = 1
+};
+
+
+function resetGame() {
+  firstCard = null ;
+  secondCard = null;
+  moves = 0 ;
+  score = 0 ;
+  startGame();
+}
   
 
-   startGame ()
+startGame ()
    
      
         
